6c0ac36965adab36d046946d91f62ca9e2f7e9a3
Bryan <heliosbryan@gmail.com>
2016-11-07 02:51:39 -0500

    Cleanup unneeded content

--- aliases.sh
+++
@@ -1,5 +0,0 @@
-# Remove blank lines from file
-alias noblanks='grep -v "^[[:space:]]*$"'
-
-# Show only the lines with content in a config file, strip #comment lines and empty lines
-alias meat='grep -v "^#" | noblanks'
--- repo-testing/README.txt
+++
@@ -1,1 +0,0 @@
-This holds testing scripts/utils for the repository scripts
